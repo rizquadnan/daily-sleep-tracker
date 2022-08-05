9df8cdc29cfa3894e3026efa6105a13768814774
Muhammad Adnan R <madnanrizqullah@gmail.com>
2022-08-05 08:35:40 +0700

    chore: update px to rem utils

--- src/utils/style.test.ts
+++ src/utils/style.test.ts
@@ -4,4 +4,12 @@ describe("pxToRem", () => {
   it("converts px to rem", () => {
     expect(pxToRem(16)).toBe("1rem");
   });
+
+  it("converts 0 px to 0 rem", () => {
+    expect(pxToRem(0)).toBe("0rem");
+  });
+
+  it("converts negative px to 0 rem", () => {
+    expect(pxToRem(-1)).toBe("0rem");
+  });
 });
--- src/utils/style.ts
+++ src/utils/style.ts
@@ -1,3 +1,6 @@
 import { BASE_FONT_SIZE } from "theme";
 
-export const pxToRem = (px: number) => `${px / BASE_FONT_SIZE}rem`;
+export const pxToRem = (px: number) => {
+  if (px < 0) return "0rem";
+  else return `${px / BASE_FONT_SIZE}rem`;
+};
