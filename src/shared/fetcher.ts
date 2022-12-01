8db1cf6731fc73593c38d470a34954648beafec1
Muhammad Adnan R <madnanrizqullah@gmail.com>
2022-12-02 06:10:14 +0700

    feat: add axios interceptor for token

--- src/shared/fetcher.ts
+++ src/shared/fetcher.ts
@@ -1,6 +1,19 @@
 import axios from "axios";
+import { getToken } from "utils/session";
 
-export const fetcher = axios.create({
-  baseURL: "http://localhost:3000",
+const fetcher = axios.create({
+  baseURL: "http://localhost:3000/api/v1",
   timeout: 1000,
 });
+
+fetcher.interceptors.request.use(function (config) {
+  const token = getToken();
+
+  if (config.headers && token) {
+    config.headers.Authorization = token;
+  }
+
+  return config;
+});
+
+export { fetcher };
