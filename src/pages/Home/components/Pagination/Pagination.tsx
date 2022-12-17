3ceb06167fed397afb5d437f1e4d7c8553fd8b39
Muhammad Adnan R <madnanrizqullah@gmail.com>
2022-12-17 17:45:27 +0700

    feat: add currentpage display

--- src/pages/Home/components/Pagination/Pagination.tsx
+++ src/pages/Home/components/Pagination/Pagination.tsx
@@ -1,5 +1,5 @@
 import { ArrowBackIcon, ArrowForwardIcon } from '@chakra-ui/icons'
-import { ButtonGroup, IconButton } from '@chakra-ui/react'
+import { HStack, IconButton, Text } from '@chakra-ui/react'
 
 type PaginationProps = {
   page: number
@@ -13,20 +13,21 @@ const isLastPage = (page: number, totalPage: number) => page === totalPage
 
 function Pagination(props: PaginationProps) {
   return (
-    <ButtonGroup>
+    <HStack>
       <IconButton
         icon={<ArrowBackIcon />}
         aria-label="Previous page"
         disabled={isFirstPage(props.page)}
         onClick={props.onPrev}
       />
+      <Text>{props.page}</Text>
       <IconButton
         icon={<ArrowForwardIcon />}
         aria-label="Next page"
         disabled={isLastPage(props.page, props.totalPage)}
         onClick={props.onNext}
       />
-    </ButtonGroup>
+    </HStack>
   )
 }
 
