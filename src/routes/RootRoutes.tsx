6d58f0ebfa56e90c05e8d7fdf3968d9a3301cbe3
Muhammad Adnan R <madnanrizqullah@gmail.com>
2022-12-30 07:48:22 +0700

    feat: remove dev navigation

--- src/routes/RootRoutes.tsx
+++ src/routes/RootRoutes.tsx
@@ -1,51 +1,24 @@
-import { Heading, Link, Wrap } from '@chakra-ui/react'
-import { Routes, Route, Link as RouterLink } from 'react-router-dom'
+import { Heading } from '@chakra-ui/react'
+import { Routes, Route } from 'react-router-dom'
 import { Home, Login, Register, SplashScreen } from 'pages'
 import { ROUTES } from './constant'
 import { ProtectedRoute } from './ProtectedRoute'
 
 export function RootRoutes() {
   return (
-    <div>
-      <nav>
-        <Wrap as="ul" position="fixed" top="0" left="0">
-          <li>
-            <Link as={RouterLink} to={ROUTES.splashScreen}>
-              Splash Screen
-            </Link>
-          </li>
-          <li>
-            <Link as={RouterLink} to={ROUTES.login}>
-              Login
-            </Link>
-          </li>
-          <li>
-            <Link as={RouterLink} to={ROUTES.register}>
-              Register
-            </Link>
-          </li>
-          <li>
-            <Link as={RouterLink} to={ROUTES.home}>
-              Home
-            </Link>
-          </li>
-        </Wrap>
-      </nav>
-
-      <Routes>
-        <Route path={ROUTES.login} element={<Login />} />
-        <Route path={ROUTES.register} element={<Register />} />
-        <Route
-          path={ROUTES.home}
-          element={
-            <ProtectedRoute>
-              <Home />
-            </ProtectedRoute>
-          }
-        />
-        <Route path={ROUTES.splashScreen} element={<SplashScreen />} />
-        <Route path="*" element={<Heading>Not found</Heading>} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path={ROUTES.login} element={<Login />} />
+      <Route path={ROUTES.register} element={<Register />} />
+      <Route
+        path={ROUTES.home}
+        element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        }
+      />
+      <Route path={ROUTES.splashScreen} element={<SplashScreen />} />
+      <Route path="*" element={<Heading>Not found</Heading>} />
+    </Routes>
   )
 }
