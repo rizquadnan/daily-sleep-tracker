45783cf3e13daa2cfc9128956a1c61dd6cf8d21a
Muhammad Adnan R <madnanrizqullah@gmail.com>
2023-01-05 06:51:54 +0700

    fix: change axios timeout

--- src/api/fetcher.ts
+++ src/api/fetcher.ts
@@ -3,7 +3,7 @@ import { getToken } from "utils/session";
 
 const fetcher = axios.create({
   baseURL: "http://localhost:3000/api/v1",
-  timeout: 1000,
+  timeout: 10000,
 });
 
 fetcher.interceptors.request.use(function (config) {
